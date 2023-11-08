ac0b28051165116cd199329c8b7c7b30130cdf33
Frankge23 <130200419+Frankge23@users.noreply.github.com>
2023-11-07 19:04:11 -0600

    Guia4

---
+++ Guia4/SistemaHotelFrank/src/java/Servlet/InicioSesion.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author OA
+ */
+@WebServlet(name = "InicioSesion", urlPatterns = {"/InicioSesion"})
+public class InicioSesion extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ("usuario".equals(username) && "contrasena".equals(password)) {            
+            String mensaje = "Inicio de sesión exitoso para el usuario: " + username;
+            response.setContentType("text/html");
+            response.getWriter().println("<html><body>");
+            response.getWriter().println("<h1>" + mensaje + "</h1>");
+            response.getWriter().println("</body></html>");
+        } else {
+            String mensaje = "Error: Nombre de usuario o contraseña incorrectos";
+            response.setContentType("text/html");
+            response.getWriter().println("<html><body>");
+            response.getWriter().println("<h1>" + mensaje + "</h1>");
+            response.getWriter().println("</body></html>");
+        }
+        response.sendRedirect("error404.html");
+    }
+}
+
